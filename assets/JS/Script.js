cef1a8bd08b24e749873ef7a5aef8fc268c0e987
JulianBenchimol <jbenchimol1999@gmail.com>
2022-10-19 06:59:48 -0400

    Started working on api integrations

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,6 +1,12 @@
 var dateTimeElement;
 var now;
-
+//var fetchLink = "api.openweathermap.org/data/2.5/forecast/daily?lat=41.524300&lon=72.075821&cnt=5&appid=17c3c512eba29f875a67c16fb6f3765b"
+$.ajax({
+    url: 'http://api.openweathermap.org/geo/1.0/direct?q=Norwich,CT,3166-2:US&limit=3&appid=17c3c512eba29f875a67c16fb6f3765b',
+    method: 'GET'
+}).then(function(response){
+    console.log(response);
+})
 var UpdateTime = function(){
     now = moment();
     dateTimeElement.text(now.format('dddd, MMMM Do YYYY, h:mm a'));
